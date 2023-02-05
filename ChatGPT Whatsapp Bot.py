16d57b37bbdf54993b6a3ad717e11137e13b7d48
Zhang Xiaohui <50721951+ZhangXiaohui07@users.noreply.github.com>
2023-02-05 10:49:34 +0700

    Creating a Whatsapp ChatGPT Bot using Python
     Creating a Whatsapp ChatGPT Bot using Python
    
    Here are the general steps to integrate ChatGPT with WhatsApp using a Twilio number and Python:
    
        Get an API Key from OpenAI.
        Create a Twilio account and obtain a Twilio phone number.
        Use the Twilio API to receive incoming messages from WhatsApp and send outgoing messages to WhatsApp.
        Connect the OpenAI API Key with the Twilio API to allow ChatGPT to respond to incoming messages.
        Write a script to determine how ChatGPT will respond to messages received through WhatsApp via Twilio.
        Test the integration by sending messages to your Twilio number and ensuring that ChatGPT can respond correctly
    
    Note:
    You'll need to replace the placeholder values with your own OpenAI API key, Twilio account SID, auth token, and Twilio number. You can run the code using a web framework like Flask to handle incoming messages from Twilio. When a message is received, the code will generate a response from ChatGPT and send it back to the sender using the Twilio API.
    To run this file, you need to host it on a server that is accessible through the internet. Then, you need to configure Twilio to send incoming messages to your server's URL.

--- ChatGPT Whatsapp Bot.py
+++ ChatGPT Whatsapp Bot.py
@@ -1,10 +1,16 @@
 import openai
 import requests
 import json
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+from flask import Flask, request
 
 # Initialize the OpenAI API Key
 openai.api_key = "your_openai_api_key"
+
+# Initialize the Twilio API Client
+account_sid = "your_twilio_account_sid"
+auth_token = "your_twilio_auth_token"
+client = Client(account_sid, auth_token)
 
 # Function to generate a response from ChatGPT
 def generate_response(prompt):
@@ -18,18 +24,30 @@ def generate_response(prompt):
     )
     message = completions.choices[0].text
     return message
-
+    
 # Function to handle incoming messages from Twilio
 def handle_message(request):
     # Extract the message text from the request
     incoming_message = request.values.get("Body", None)
+    sender = request.values.get("From", None)
 
     # Use the incoming message to generate a response from ChatGPT
     response_message = generate_response(incoming_message)
+    
+    # Send the response back to the user via Twilio's WhatsApp API
+    message = client.messages.create(
+        to="whatsapp:" + sender,
+        from_="whatsapp:your_twilio_whatsapp_number",
+        body=response_message
+    )
+    
+    # Return a success status code
+    return ("", 204)
 
-    # Return the response as a Twilio XML response
-    resp = MessagingResponse()
-    resp.message(response_message)
-    return str(resp)
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Add a route for incoming messages
+app.add_route("/incoming", handle_message, methods=["POST"])
 
 # Save the file as app.py or another name of your choice
